54608726a230b06355a03e85d8b4036b62737887
geostojanov <gestojanov@gmail.com>
2022-12-09 07:07:34 +0100

    loggin users out

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,17 +4,37 @@
       <img src="@/assets/ninja.png">
       <h1><router-link :to="{ name: 'home' }">Muso Ninjas</router-link></h1>
       <div class="links">
-        <button>Logout</button>
-        <router-link class="btn" :to="{ name: 'Signup' }">Signup</router-link>
-        <router-link class="btn" :to="{ name: 'Login' }">Log in</router-link>
+        <div v-if="user">
+          <button @click="handleClick">Logout</button>
+        </div>
+        <div v-else>
+          <router-link class="btn" :to="{ name: 'Signup' }">Signup</router-link>
+          <router-link class="btn" :to="{ name: 'Login' }">Log in</router-link>
+        </div>
       </div>
     </nav>
   </div>
 </template>
 
 <script>
-export default {
+import useLogout from '@/composables/useLogout'
+import getUser from '@/composables/getUser'
+import { useRouter } from 'vue-router'
 
+export default {
+  setup() {
+    const { user } = getUser()
+    const { logout } = useLogout()
+    const router = useRouter()
+    
+    const handleClick = async () => {
+      await logout()
+      console.log('user logged out')
+      router.push({ name: 'Login' })
+    }
+    
+    return { handleClick, user }
+  }
 }
 </script>
 
