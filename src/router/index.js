435dfa5d46e73ef5905607e730d22a6fd9ef2f8e
geostojanov <gestojanov@gmail.com>
2022-12-16 06:59:12 +0100

    navbar and route guard

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,21 @@ import Login from '../views/auth/Login.vue'
 import Signup from '../views/auth/Signup.vue'
 import CreatePlaylist from '../views/playlists/CreatePlaylist.vue'
 
+// route guard
+import { projectAuth } from '@/firebase/config'
+
+const requireAuth = (to, from, next) => {
+  let user = projectAuth.currentUser
+  if (!user) next({ name: 'Login' })
+  else next()
+}
+
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    beforeEnter: requireAuth
   },
   {
     path: '/login',
@@ -23,7 +33,8 @@ const routes = [
   {
     path: '/playlists/create',
     name: 'CreatePlaylist',
-    component: CreatePlaylist
+    component: CreatePlaylist,
+    beforeEnter: requireAuth
   },
 ]
 
